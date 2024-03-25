4ba69972e93424cdfcec64fa60df273948cbe80b
Aurorn <aurornispro@gmail.com>
2024-03-25 16:01:58 -0400

    base JS

---
+++ script.js
@@ -0,0 +1,21 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return(title +" by " + author + ", " + pages + ", " + read);
+    };
+}
+
+function addBookToLibrary() {
+    
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295 Pages', 'not read');
+Object.getPrototypeOf(theHobbit) === Book.prototype;
+
+console.log(theHobbit.info());
+
